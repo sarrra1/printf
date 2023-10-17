11dc736ce2a58752f90b8d8e881eb1d38548a846
dania nasraldein <danyahnasraldein@gmail.com>
2023-10-17 15:38:53 -0700

    up

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 #include <limits.h>
 
 int _printf(const char *format, ...);
-int handle_to_printf(const char *format, int *i, va_list list_args_print);
 int handle_to_print_integer(int sara_int);
+int _putchar(char x);
 
 #endif
