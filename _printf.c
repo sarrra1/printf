7c4a42909d2a86b1665518655f109841558f1ffb
sarrra1 <sarafaisal0161999@gmail.com>
2023-10-19 08:03:34 -0700

    print up

--- _printf.c
+++ _printf.c
@@ -10,49 +10,60 @@ int _printf(const char *format, ...)
 va_list args;
 va_start(args, format);
 int print_count = 0;
-while (*format != '\0') {
-if (*format == '%') {
+while (*format != '\0')
+{
+if (*format == '%')
+{
 format++;
-switch (*format) {
+switch (*format)
+{
 case 'd':
-case 'i': {
-int arg = va_arg(args, int);
-print_count += handle_to_print_integer(arg);
+case 'i':
+{
+int sardan = va_arg(args, int);
+print_count += handle_to_print_integer(sardan);
 break;
 }
-case 'c': {
-                    char arg = va_arg(args, int);
-                    write(1, &arg, 1);
-                    print_count++;
-                    break;
-                }
-                case 's': {
-                    char *arg = va_arg(args, char *);
-                    int i = 0;
-                    while (arg[i] != '\0') {
-                        write(1, &arg[i], 1);
-                        i++;
-                        print_count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    write(1, "%", 1);
-                    print_count++;
-                    break;
-                }
-                default: {
-                    write(1, "%", 1);
-                    write(1, format, 1);
-                    print_count += 2;
-                    break;
-                }
-            }
-        } else {
-            write(1, format, 1);
-            print_count++;
-        }
-        format++;
+case 'c':
+{
+char dan = va_arg(args, int);
+write(1, &dan, 1);
+print_count++;
+break;
+}
+case 's':
+{
+char *dansar = va_arg(args, char *);
+int i = 0;
+while (dansar[i] != '\0')
+{
+write(1, &dansar[i], 1);
+i++;
+print_count++;
+}
+break;
+}
+case '%':
+{
+write(1, "%", 1);
+print_count++;
+break;
+}
+default:
+{
+write(1, "%", 1);
+write(1, format, 1);
+print_count += 2;
+break;
+}
+}
+}
+else
+{
+write(1, format, 1);
+print_count++;
+}
+format++;
 }
 
 va_end(args);
